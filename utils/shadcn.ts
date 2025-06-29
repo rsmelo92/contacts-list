1f679c25986f56e2705143b5bc712560a7fc00bd
Rafael Melo <rsmelo_@hotmail.com>
2025-06-29 17:22:04 -0300

    test: add code to trigger CI

--- utils/shadcn.ts
+++ utils/shadcn.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getRandomColor() {
+  const colors = ['#FF5733', '#33FF57', '#3357FF', '#FF33A1', '#A133FF'];
+  return colors[Math.floor(Math.random() * colors.length)];
+}
